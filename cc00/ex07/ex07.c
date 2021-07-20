c79bfe3cd34fc3a32e65efb3ec99631d150b42e5
ch4zx1 <charles.fischer17@gmail.com>
2021-07-20 13:57:29 -0300

    revint

--- cc00/ex07/ex07.c
+++ cc00/ex07/ex07.c
@@ -7,23 +7,30 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb) 
 {
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
 	if (nb < 0) 
     {
-		ft_putchar('-');
 		nb = -nb;
-	}
-	if (nb >= 10) 
-    {
-		ft_putnbr(nb / 10);
-		nb = nb % 10;
+		ft_putchar('-');
 	}
 	if (nb < 10) 
     {
         ft_putchar(nb + '0');
     }
+	else
+    {
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	
 }
 
 int main(void)
 {
-    ft_putnbr(20);
+    ft_putnbr(-2147483648);	
 }
